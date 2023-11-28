16192fd3cfc4aaf002e230a7e02ba05aaf3bb727
Nabeel Hassan <[200~nabeelhassa02@gmail.com>
2023-11-28 22:33:21 +0500

    first commit

---
+++ form.php
@@ -0,0 +1,54 @@
+<?php
+
+//Import PHPMailer classes into the global namespace
+//These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Retrieve form data
+       
+    $firstName = $_POST["first"];
+    $lastName = $_POST["last"];
+    $email = $_POST["email"];
+    $message = $_POST["massage"];
+
+    //Load Composer's autoloader
+    require 'phpmailer/Exception.php';
+    require 'phpmailer/PHPMailer.php';
+    require 'phpmailer/SMTP.php';
+    //Create an instance; passing `true` enables exceptions
+    $mail = new PHPMailer(true);
+
+try {
+    $mail->isSMTP();                  // Send using SMTP
+    $mail->Host = 'smtpout.secureserver.net'; // Set the SMTP server to send through
+    $mail->SMTPAuth = true;           // Enable SMTP authentication
+    $mail->Username = 'info@pmdeskapp.com'; // Your Hostinger email
+    $mail->Password = 'Adsvr!@#45'; // Your email password
+    $mail->SMTPSecure = 'ssl'; // Use TLS
+    $mail->Port = 465;                // Port number for TLS
+
+    // Recipients
+    $mail->setFrom('info@pmdeskapp.com', '');
+    $mail->addAddress('info@pmdeskapp.com', 'info@pmdeskapp');
+
+    //Content
+    $mail->isHTML(true);                                  //Set email format to HTML
+    $mail->Subject = ' CONTACT US';
+    $mail->Body = "First Name: $firstName\n";
+    $mail->Body .= "Last Name: $lastName\n";
+    $mail->Body .= "Email: $email\n";
+    $mail->Body .= "Message:$message<br>";
+
+
+
+    $mail->send();
+    //    header("Location:/pelzon/#");
+       echo "Thank you for submitting the form!";
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error:{$mail->ErrorInfo}";
+}
+}
+?>
